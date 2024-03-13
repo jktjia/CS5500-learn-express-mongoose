a3cbadb824ba824f070595aee6154a44341f8e8c
jktjia <103521429+jktjia@users.noreply.github.com>
2024-03-13 16:16:15 -0400

    in class activities

--- models/author.js
+++ models/author.js
@@ -28,7 +28,17 @@ AuthorSchema
 });
 
 // Virtual for author's lifespan
-AuthorSchema.virtual('lifespan').get(function() {});
+AuthorSchema.virtual('lifespan').get(function() {
+  var lifespan = '';
+  if (this.date_of_birth) {
+    lifespan += this.date_of_birth.getYear().toString();
+  }
+  lifespan += ' - ';
+  if (this.date_of_death) {
+    lifespan += this.date_of_death.getYear().toString();
+  }
+  return lifespan;
+});
 
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
