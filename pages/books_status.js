c5d905817f14e2293e79a3ef864d418265ae793c
jktjia <103521429+jktjia@users.noreply.github.com>
2024-03-19 20:42:41 -0400

    in class activities march 18 (i forgot to commit it yesterday)

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,11 @@
 let BookInstance = require('../models/bookinstance');
 
 exports.show_all_books_status = function(res) {
-  return res.send([]);
+  return BookInstance.find({ status: {$eq: 'Available'}})
+  .populate('book')
+  .then((books) => {
+    res.send(books.map((b) => {
+      return b.book.title + ' : ' + b.status;
+    }));
+  });
 }
